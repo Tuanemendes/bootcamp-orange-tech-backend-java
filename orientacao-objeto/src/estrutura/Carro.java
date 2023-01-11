34cdab77e8e6677c0290d6e2ae09ee11c67071c3
Tuanemendes <tuane.belmiro@gmail.com>
2023-01-11 20:51:39 -0300

    criação de classe para POO

---
+++ orientacao-objeto/src/estrutura/Carro.java
@@ -0,0 +1,43 @@
+package estrutura;
+
+public class Carro {
+    String cor;
+    String modelo;
+    int capacidadeTanque;
+
+    Carro(){
+
+    }
+    Carro(String cor , String modelo, int capacidadeTanque){
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+    }
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    public void setCapacidadeTanque(int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    public double totalValorTanque(double valorCombustivel){
+        return capacidadeTanque * valorCombustivel;
+    }
+}
---
+++ orientacao-objeto/src/fundamentos/Livros.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class Livros {
+
+    private String titulo;
+    private String autor;
+    private String editora;
+    private String iscn;
+    private int quantidaPaginas;
+
+
+
+
+}
