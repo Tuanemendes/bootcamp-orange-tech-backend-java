4b15a092ca1453f220e4d0ead0e40d53cee43d19
Tuanemendes <tuane.belmiro@gmail.com>
2023-01-14 17:25:49 -0300

    colletions com list

---
+++ colletions-java/src/list/ExemploList.java
@@ -0,0 +1,12 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExemploList {
+    public static void main(String[] args) {
+        List<Double> notas = new ArrayList<>();
+
+    }
+
+}
