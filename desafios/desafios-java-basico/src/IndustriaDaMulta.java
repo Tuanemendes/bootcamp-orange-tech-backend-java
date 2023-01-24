0cd1ee0ae040e75d99067032c5162d80d3e2799c
Tuanemendes <tuane.belmiro@gmail.com>
2023-01-24 20:41:52 -0300

    implementação no desafio

--- desafios/desafios-java-basico/src/IndustriaDaMulta.java
+++ desafios/desafios-java-basico/src/IndustriaDaMulta.java
@@ -4,11 +4,12 @@ public class IndustriaDaMulta {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         int velocidadeAtual = leitor.nextInt();
-        if (velocidadeAtual <= 60) {
+        System.out.println((velocidadeAtual<= 60 )? "Nao foi multado" : "Foi multado");
+        /*if (velocidadeAtual <= 60) {
             System.out.println("Nao foi multado");
         } else {
             System.out.println("Foi multado");
-        }
+        }*/
 
     }
 }
